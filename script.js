21a543c33761cb4161e25ac8fada8ab17dcf96f4
Muhammad <muhammadbm.01@gmail.com>
2025-08-02 00:16:53 -0400

    Complete

--- script.js
+++ script.js
@@ -30,15 +30,35 @@ function displayBooks() {
   myLibrary.forEach(book => {
     const card = document.createElement("div");
     card.className = "book-card";
+    card.dataset.id = book.id;
+
     card.innerHTML = `
       <h3>${book.title}</h3>
       <p><strong>Author:</strong> ${book.author}</p>
       <p><strong>Pages:</strong> ${book.pages}</p>
       <p><strong>Status:</strong> ${book.isRead ? 'Read' : 'Not read'}</p>
     `;
+
+    card.appendChild(createToggleReadButton(book));
+    card.appendChild(createRemoveButton(book.id));
     bookList.appendChild(card);
   })
 };
+
+function createRemoveButton(bookId) {
+  const btn = document.createElement("button");
+  btn.textContent = "Remove";
+  btn.className = "remove-book-btn";
+
+  btn.addEventListener("click", function() {
+    const index = myLibrary.findIndex(book => book.id === bookId);
+    if (index !== -1) {
+      myLibrary.splice(index, 1);
+      displayBooks();
+    }
+  });
+  return btn;
+}
 
 displayBooks();
 
@@ -53,3 +73,40 @@ showBtn.addEventListener("click", () => {
 cancelBtn.addEventListener("click", () => {
   dialog.close();
 });
+
+newBookInfo();
+
+function newBookInfo() {
+  document.getElementById("add-book-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const isRead = document.getElementById("isRead").checked;
+
+    console.log(title, author, pages, isRead);
+
+    addBookToLibrary(title, author, pages, isRead);
+    displayBooks();
+    this.reset();
+    dialog.close();
+  })
+};
+
+Book.prototype.toggleRead = function() {
+  this.isRead = !this.isRead;
+}
+
+function createToggleReadButton(book) {
+  const btn = document.createElement("button");
+  btn.textContent = "Toggle Read";
+  btn.className = "toggle-read-btn";
+
+  btn.addEventListener("click", function() {
+    book.toggleRead();
+    displayBooks();
+  });
+  return btn;
+}
